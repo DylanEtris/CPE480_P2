cd1e8433089f2d833f8b78f098e06ff251e0c71c
DylanEtris <61713152+DylanEtris@users.noreply.github.com>
2020-03-02 16:48:33 -0500

    Update project2_v1.v
    Added some opcode definitions

--- project2_v1.v
+++ project2_v1.v
@@ -1,20 +1,27 @@
 // Important State Machine States
-'define Start	5'b0000
-'define Decode  5'b0001
+'define Start		4'b0001
+'define Decode  	4'b0002
 
 // Basic sizes
-'define STATE	[3:0]
-'define WORD	[15:0]
-'define MEMSIZE [65535:0]	// Total amount of instructions in memory
-'define REGSIZE [7:0]		// Number of Registers
+'OPSIZE			[7:0]
+'define STATE		[3:0]
+'define WORD		[15:0]
+'define MEMSIZE 	[65535:0]	// Total amount of instructions in memory
+'define REGSIZE 	[7:0]		// Number of Registers
 
 //Instruction Field Placements
 //NOTE: I could be wrong about these, I generated them from Dr. Deitz's assembler implementation
-'define Op0	[15:12]
-'define Op1	[11:8]
-'define Reg0	[3:0]
-'define Reg1	[7:4]
-'define Imm8	[11:4]
+'define Op0		[15:12]
+'define Op1		[11:8]
+'define Reg0		[3:0]
+'define Reg1		[7:4]
+'define Imm8		[11:4]
+
+//Instruction Codes
+'define jrOrTrap	4'b0000
+'define not 		4'b0001
+'define conversions	4'b0010
+'define anyOrNeg	4'b0011
 
 // TODO: complete ALU
 module alu()
@@ -25,7 +32,8 @@ module processor(halt, reset, clk);
 	//control signal definitions
 	output reg halt;
 	input reset, clk;
-	reg 'STATE s, op
+	reg 'STATE s;
+	reg 'OPSIZE op;
 
 	//processor component definitions
 	reg 'WORD text'MEMSIZE;		// instruction memory
@@ -53,8 +61,9 @@ module processor(halt, reset, clk);
 			'Decode: 
 				begin // TODO: Figure out how to assign state s to procede with next step.
 					pc <= pc + 1;
-					op <= ir 'Op0;
+					op <= {ir 'Op0, ir 'Op1};
 					rn <= ir 'Reg0;
+					s  <= 
 				end
 			// TODO: add other state cases like jumps, branches, and arithmetic operations.
 			default: begin sys <= ir 'Imm8; halt <= 1; end; // halts the program and saves the current instruction
