c653618bb17401c4a940ecb642b6860cfd0b64df
DylanEtris <61713152+DylanEtris@users.noreply.github.com>
2020-03-03 20:02:25 -0500

    Update project2_v1.v
    Added trap cases.

--- project2_v1.v
+++ project2_v1.v
@@ -61,11 +61,19 @@
 `define OPsltii		8'b01110111
 
 // TODO: complete ALU
-module alu(rd, rs, op, out);
+module alu(rd, rs, op, aluOut, aluTrap);
 	input `WORD rd;
 	input wire `WORD rs;
 	input wire `OPSIZE op;
-	output wire `WORD out;
+	output wire `WORD aluOut;
+	output wire trap;
+	
+	reg `WORD out;
+	assign aluOut = out;
+	
+	reg trap;
+	assign aluTrap = trap;
+	
 	
 	//These are the operations 
 	always @* begin 
@@ -90,10 +98,10 @@ module alu(rd, rs, op, out);
 				out = rd[15:8] < rs[15:8]; 
 				rd[7:0] = rd[7:0] < rs[7:0]; 
 			end
-			`OPaddp: begin end
-			`OPaddpp: begin end
-			`OPmulp: begin end
-			`OPmulpp: begin end
+			`OPaddp: begin assign trap = 1; end
+			`OPaddpp: begin trap = 1; end
+			`OPmulp: begin trap = 1; end
+			`OPmulpp: begin trap = 1; end
 			`OPand: begin out = rd & rs; end
 			`OPor: begin out = rd | rs; end
 			`OPxor: begin out = rd ^ rs; end
@@ -104,12 +112,12 @@ module alu(rd, rs, op, out);
 				out `HighBits = ~rd `HighBits; 
 				out `LowBits = ~rd `LowBits; 
 			end
-			`OPi2p: begin end
-			`OPii2pp: begin end
-			`OPp2i: begin end
-			`OPpp2ii: begin end
-			`OPinvp: begin end
-			`OPinvpp: begin end
+			`OPi2p: begin trap = 1; end
+			`OPii2pp: begin trap = 1; end
+			`OPp2i: begin trap = 1; end
+			`OPpp2ii: begin trap = 1; end
+			`OPinvp: begin trap = 1; end
+			`OPinvpp: begin trap = 1; end
 			`OPnot: begin out = ~rd; end	
 		endcase	
 	end
@@ -129,8 +137,8 @@ module processor(halt, reset, clk);
 	reg `WORD ir;
 	reg `WORD regfile `REGSIZE;		// Register File Size
 	reg `WORD rd;
-	wire `WORD rs;
-	alu myalu(rd, rs, op, aluOut);
+	wire `WORD rs, aluOut, trap;
+	alu myalu(rd, rs, op, aluOut, trap);
 
 	//processor initialization
 	always @(posedge reset) begin
@@ -162,7 +170,6 @@ module processor(halt, reset, clk);
 			`Decode: 
 				begin // TODO: Figure out how to assign state s to procede with next step.
 					op <= {ir `Op0, ir `Op1};
-					rn <= ir `Reg0;
 					s  <= ir `Op0;
 				end
 			`LdOrSt:
